1cecb00fe72419592d78cc6d11dcddf4cd23ce88
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-29 18:19:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -150,7 +150,23 @@ if (!$result1){
                 </div>
                 <div class="list w3-row">
                     <div class="" id="Lego"><h2>Lego</h2>
-                    
+                    <?php
+     include './dbconnector.php';
+     $query = "SELECT iid, iname, idescription, iprice, istatus, iimage,cName FROM Item,Catalogue WHERE Item.cid=Catalogue.cid AND cname LIKE '%Lego%'  ORDER BY cName";
+     $result = pg_query($query);
+     $error = $msg = "";
+     if (!$result){
+      $error = "Couldn't load data, please try again.";
+     }
+     while ($row = pg_fetch_assoc($result)) 
+     {
+        $iid = $row['iid'];
+        $iname = $row['iname'];
+        $idescription = $row['idescription'];
+        $iprice = $row['iprice'];
+        $istatus = $row['istatus'];
+        $iimage = $row['iimage'];
+        ?>        
         
      
                 </div>        
