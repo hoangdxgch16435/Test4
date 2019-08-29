2a2ade078dae4c5feb1d5676c2f8f8a0bd9fff62
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-29 18:51:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -166,30 +166,12 @@ if (!$result1){
         $iprice = $row['iprice'];
         $istatus = $row['istatus'];
         $iimage = $row['iimage'];
-      echo "<div class='sp w3-quarter w3-card w3-center ' ><div class='w3-orange w3-padding-large'>$iStatus</div><div ><img onclick=\"document.getElementById('$iName').style.display='block'\" id='testimg' src='". $iimage . "' width='100%'></div><div class='name'><h3>$iname</h3></div><h3>$iprice$</h3></div>"
-                . "<!--SHOW MORE INFORMATION-->
-  <div id='$iname' class='w3-modal'>
-      <div class='w3-modal-content w3-animate-top w3-card-4'>
-        <div class='w3-container w3-orange w3-center w3-padding-20'> 
-          <span onclick=\"document.getElementById('$iname').style.display='none';\"
-         class='w3-button w3-orange w3-xlarge w3-display-topright'>×</span>
-          <h2>$iname</h2>
-        </div>
-        <div class='w3-container w3-row'>
-          <div class='w3-half'>
-              <img src='". $iimage . "' width='100%'>
-          </div>
-          <div class='w3-half w3-left'>
-              <h3> Price: $iprice$</h3>
-              <p> Description: $idescription.</p>
-              <h4></h4>                           
-          </div>                                                    
-        </div>
-        <button class='w3-button w3-orange w3-section' onclick=\"document.getElementById('$iName').style.display='none';\">Close <i class='fa fa-remove'></i></button>
-      </div>
-    </div>";
+      
+
   }
-        ?>        
+        ?>  
+              <a> <img src="<?= $iimage; ?>"  width="220px" height="200px" />
+          <h3><?= $iname; ?></h3> </a>      
         
      
                 </div>        
